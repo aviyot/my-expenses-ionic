f2cdee43d13861dce20dfa83f7b396ff4a6c05d0
aviyot <aviyot@gmail.com>
2021-11-01 20:57:15 +0200

    change defaulat langue to hebrew

--- src/app/services/language/language.service.ts
+++ src/app/services/language/language.service.ts
@@ -1,18 +1,21 @@
 import { Injectable } from "@angular/core";
-import { BehaviorSubject, Observable} from 'rxjs';
+import { BehaviorSubject, Observable } from "rxjs";
 
-import languages from "../../../assets/languages/languages"
+import languages from "../../../assets/languages/languages";
 
 @Injectable({
   providedIn: "root",
 })
 export class LanguageService {
-  languages = languages
-  private _selectedLanguage:BehaviorSubject<any> = new BehaviorSubject(this.languages["english"]);
-  public selectedLanguage:Observable<string> = this._selectedLanguage.asObservable();
+  languages = languages;
+  private _selectedLanguage: BehaviorSubject<any> = new BehaviorSubject(
+    this.languages["hebrew"]
+  );
+  public selectedLanguage: Observable<string> =
+    this._selectedLanguage.asObservable();
 
   constructor() {}
-  changeLang(language:string){
+  changeLang(language: string) {
     this._selectedLanguage.next(this.languages[language]);
   }
 }
