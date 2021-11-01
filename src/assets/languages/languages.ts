8f53d440f87af3aa72020c7a2a2f7e62928e1814
aviyot <aviyot@gmail.com>
2021-11-01 20:58:59 +0200

    add type to languages

--- src/assets/languages/languages.ts
+++ src/assets/languages/languages.ts
@@ -1,94 +1,191 @@
-const languages = {
-    hebrew: {
-      title: "הוצאות שלי",
-      totalExpenses: 'סה"כ',
-      sortBy: "מיין לפי",
-      cancel: "בטל",
-      ok: "אשר",
-      acs: "סדר עולה",
-      des: "סדר יורד",
-      addNewExp: "הוסף הוצאה חדשה",
-      optList: "עריכת אפשריות",
-      edit: "ערוך",
-      editCategoery: "ערוך קטגוריות",
-      editExpensesMethod: "ערוך שיטת תשלום",
-      amount: "סכום",
-      name: "שם",
-      category: "קטגוריה",
-      payMethod: "שיטת תשלום",
-      fristPayDate: "תשלום ראשון",
-      lastPayDate: "תשלום אחרון",
-      numberPays: "מספר תשלומים",
-      freqPay:"תדירות תשלום",
-      beneficiary: "המוטב",
-      commitTime: "תאריך העסקה",
-      update: "עדכן",
-      enter: "הכנס",
-      new: "חדש",
-      pay: "תשלום",
-      opthion: "אפשרות",
-      select: "בחר",
-      add:"הוסף",
-      sortAcs:"סדר עולה",
-      sortDcs:"סדר יורד",
-      enterRequired:"הכנס נתונים נדרשים",
-      dataAdded:"הנתונים נשמרו בהצלחה",
-      del:"הסר",
-      opthionEdit:"עריכת אפשרויות",
-      editExpenseCat:"ערוך קטגורית הוצאות",
-      editExpenseMethod:"ערוך סוג התשלום",
-      enterCat:"הכנס קטגוריה חדשה",
-      enterMethod:"הכנס סוג תשלום חדש",
-      menu:"תפריט",
-      english:"אנגלית",
-      hebrew:"עברית",
-      language:"שפה"
-    },
-    english: {
-      title:"my expenses",
-      totalExpenses:"total",
-      sortBy: "Sort By",
-      cancel: "Cancel",
-      ok: "Ok",
-      acs: "acs",
-      des: "Des",
-      addNewExp: "Add New Expenses",
-      optList: "Opthion list",
-      edit: "Edit",
-      editCategoery: "Edit Categoery",
-      editExpensesMethod: "Edit Expense Method",
-      amount: "Amount",
-      name: "Name",
-      category: "Category",
-      payMethod: "Pay Method",
-      fristPayDate: "Frist Pay Date",
-      lastPayDate: "Last Pay Date",
-      numberPays: "Number Of Pays",
-      freqPay:"Frequency Payment",
-      beneficiary: "Beneficiary",
-      commitTime: "Commit Time",
-      update: "Update",
-      enter: "Enter",
-      addNew: "Add New",
-      pay: "Pay",
-      opthion: "Opthion", 
-      select: "Select",
-      add:"add",
-      sortAcs:"Sort Acs",
-      sortDcs:"Sort Dcs",
-      enterRequired:"Enter Required Data",
-      dataAdded:"Data Added successfully",
-      del:"Delete",
-      opthionEdit:"Opthions Edit",
-      editExpenseCat:"Edit Expenses Category",
-      editExpenseMethod:"Edit Expense Pay Method",
-      enterCat:"Enter Category",
-      enterMethod:"Enter pay method",
-      menu:"Menu",
-      english:"English",
-      hebrew:"Hebrew",
-      language:"Language"
-    }
-  }
+interface Language {
+  name: string;
+  entries: {
+    title: string;
+    totalExpenses: string;
+    sortBy: string;
+    cancel: string;
+    ok: string;
+    acs: string;
+    des: string;
+    addNewExp: string;
+    optList: string;
+    edit: string;
+    editCategoery: string;
+    editExpensesMethod: string;
+    amount: string;
+    name: string;
+    category: string;
+    payMethod: string;
+    fristPayDate: string;
+    lastPayDate: string;
+    numberPays: string;
+    freqPay: string;
+    beneficiary: string;
+    commitTime: string;
+    update: string;
+    enter: string;
+    addNew: string;
+    pay: string;
+    opthion: string;
+    select: string;
+    add: string;
+    sortAcs: string;
+    sortDcs: string;
+    enterRequired: string;
+    dataAdded: string;
+    del: string;
+    opthionEdit: string;
+    editExpenseCat: string;
+    editExpenseMethod: string;
+    enterCat: string;
+    enterMethod: string;
+    menu: string;
+    english: string;
+    hebrew: string;
+    language: string;
+  };
+}
 
-  export default languages
+const hebrewLang: Language = {
+  name: "hebrew",
+  entries: {
+    title: "הוצאות שלי",
+    totalExpenses: 'סה"כ',
+    sortBy: "מיין לפי",
+    cancel: "בטל",
+    ok: "אשר",
+    acs: "סדר עולה",
+    des: "סדר יורד",
+    addNewExp: "הוסף הוצאה חדשה",
+    optList: "עריכת אפשריות",
+    edit: "ערוך",
+    editCategoery: "ערוך קטגוריות",
+    editExpensesMethod: "ערוך שיטת תשלום",
+    amount: "סכום",
+    name: "שם",
+    category: "קטגוריה",
+    payMethod: "שיטת תשלום",
+    fristPayDate: "תשלום ראשון",
+    lastPayDate: "תשלום אחרון",
+    numberPays: "מספר תשלומים",
+    freqPay: "תדירות תשלום",
+    beneficiary: "המוטב",
+    commitTime: "תאריך העסקה",
+    update: "עדכן",
+    enter: "הכנס",
+    addNew: "חדש",
+    pay: "תשלום",
+    opthion: "אפשרות",
+    select: "בחר",
+    add: "הוסף",
+    sortAcs: "סדר עולה",
+    sortDcs: "סדר יורד",
+    enterRequired: "הכנס נתונים נדרשים",
+    dataAdded: "הנתונים נשמרו בהצלחה",
+    del: "הסר",
+    opthionEdit: "עריכת אפשרויות",
+    editExpenseCat: "ערוך קטגורית הוצאות",
+    editExpenseMethod: "ערוך סוג התשלום",
+    enterCat: "הכנס קטגוריה חדשה",
+    enterMethod: "הכנס סוג תשלום חדש",
+    menu: "תפריט",
+    english: "אנגלית",
+    hebrew: "עברית",
+    language: "שפה",
+  },
+};
+const languages = {
+  hebrew: {
+    title: "הוצאות שלי",
+    totalExpenses: 'סה"כ',
+    sortBy: "מיין לפי",
+    cancel: "בטל",
+    ok: "אשר",
+    acs: "סדר עולה",
+    des: "סדר יורד",
+    addNewExp: "הוסף הוצאה חדשה",
+    optList: "עריכת אפשריות",
+    edit: "ערוך",
+    editCategoery: "ערוך קטגוריות",
+    editExpensesMethod: "ערוך שיטת תשלום",
+    amount: "סכום",
+    name: "שם",
+    category: "קטגוריה",
+    payMethod: "שיטת תשלום",
+    fristPayDate: "תשלום ראשון",
+    lastPayDate: "תשלום אחרון",
+    numberPays: "מספר תשלומים",
+    freqPay: "תדירות תשלום",
+    beneficiary: "המוטב",
+    commitTime: "תאריך העסקה",
+    update: "עדכן",
+    enter: "הכנס",
+    new: "חדש",
+    pay: "תשלום",
+    opthion: "אפשרות",
+    select: "בחר",
+    add: "הוסף",
+    sortAcs: "סדר עולה",
+    sortDcs: "סדר יורד",
+    enterRequired: "הכנס נתונים נדרשים",
+    dataAdded: "הנתונים נשמרו בהצלחה",
+    del: "הסר",
+    opthionEdit: "עריכת אפשרויות",
+    editExpenseCat: "ערוך קטגורית הוצאות",
+    editExpenseMethod: "ערוך סוג התשלום",
+    enterCat: "הכנס קטגוריה חדשה",
+    enterMethod: "הכנס סוג תשלום חדש",
+    menu: "תפריט",
+    english: "אנגלית",
+    hebrew: "עברית",
+    language: "שפה",
+  },
+  english: {
+    title: "my expenses",
+    totalExpenses: "total",
+    sortBy: "Sort By",
+    cancel: "Cancel",
+    ok: "Ok",
+    acs: "acs",
+    des: "Des",
+    addNewExp: "Add New Expenses",
+    optList: "Opthion list",
+    edit: "Edit",
+    editCategoery: "Edit Categoery",
+    editExpensesMethod: "Edit Expense Method",
+    amount: "Amount",
+    name: "Name",
+    category: "Category",
+    payMethod: "Pay Method",
+    fristPayDate: "Frist Pay Date",
+    lastPayDate: "Last Pay Date",
+    numberPays: "Number Of Pays",
+    freqPay: "Frequency Payment",
+    beneficiary: "Beneficiary",
+    commitTime: "Commit Time",
+    update: "Update",
+    enter: "Enter",
+    addNew: "Add New",
+    pay: "Pay",
+    opthion: "Opthion",
+    select: "Select",
+    add: "add",
+    sortAcs: "Sort Acs",
+    sortDcs: "Sort Dcs",
+    enterRequired: "Enter Required Data",
+    dataAdded: "Data Added successfully",
+    del: "Delete",
+    opthionEdit: "Opthions Edit",
+    editExpenseCat: "Edit Expenses Category",
+    editExpenseMethod: "Edit Expense Pay Method",
+    enterCat: "Enter Category",
+    enterMethod: "Enter pay method",
+    menu: "Menu",
+    english: "English",
+    hebrew: "Hebrew",
+    language: "Language",
+  },
+};
+
+export default languages;
